0bf542e0174416b4dd384d61c4187a0de91c094c
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-27 01:55:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string_input)
   word_array = string_input.split(" ")
   word_array = word_array.collect{ |word|
-    word = word.reverse!
-    word
+    word.reverse!
   }
   word_array.join(" ")
 end
