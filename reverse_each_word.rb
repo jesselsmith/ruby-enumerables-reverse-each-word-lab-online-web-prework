b32364f224aab0e4635716e2862c4bdac8eaa655
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-27 01:49:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string_input)
   word_array = string_input.split(" ")
   word_array = word_array.each{ |word|
-    word = word.reverse
+    word = word.reverse + " "
   }
   word_array.join
 end
